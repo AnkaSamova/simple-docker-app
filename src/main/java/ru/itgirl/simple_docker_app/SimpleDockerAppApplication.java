1d6db5b47c34a8b4c0cebeb12ea018dedff8873c
ANKA <annamaslovaspb@gmail.com>
2025-07-09 01:22:30 +0300

    week26

--- src/main/java/ru/itgirl/simple_docker_app/SimpleDockerAppApplication.java
+++ src/main/java/ru/itgirl/simple_docker_app/SimpleDockerAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleDockerAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleDockerAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleDockerAppApplication.class, args);
+    }
 }
